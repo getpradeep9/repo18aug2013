a016813b7587dd0dfee1bd205031a3931a157014
getpradeep9 <getpradeep9@gmail.com>
2013-10-18 09:37:11 +0530

    Update Main.java

--- Code/Main.java
+++ Code/Main.java
@@ -1,7 +1,7 @@
 Class Main{
 
 	public static void main(String[] args){
-	System.out.println(" Welcome to git and GitHub ");
+	System.out.println(" Welcome to git and GitHub and fetch demo");
 	}
 
 }
